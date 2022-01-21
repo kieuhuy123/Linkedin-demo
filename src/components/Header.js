ec338f0b7d509d87dd547caafc82595e9939a038
kieuhuy <kieuphuchuy2002@gmail.com>
2022-01-21 23:48:53 +0700

    update 21/1

---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+const Header = () => {
+  return <div>This is header</div>;
+};
+
+export default Header;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import Header from "./Header";
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <h1>Home</h1>
+    </>
+  );
+};
+
+export default Home;
