3cbf79ed84f01cf96a8821666c11be8f06b87a35
kieuhuy <kieuphuchuy2002@gmail.com>
2022-01-24 18:00:43 +0700

    update 21/1

---
+++ src/components/RightSide.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+const RightSide = () => {
+  return <Container>Right Side</Container>;
+};
+
+const Container = styled.div`
+  grid-area: rightside;
+`;
+export default RightSide;
